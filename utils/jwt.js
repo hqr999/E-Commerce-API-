c8cdaa50c6b2c8e492ee1e908b37cddff2ca17c9
Henrique Queiroz <henriquereuter46@gmail.com>
2025-12-12 12:59:55 -0300

    Refactored code,attachCookiesToResponse function on jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,7 @@
+const { StatusCodes } = require("http-status-codes");
 const jwt = require("jsonwebtoken");
 
-const createJWT = ({payload}) => {
+const createJWT = ({ payload }) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   });
@@ -9,7 +10,21 @@ const createJWT = ({payload}) => {
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
+const attachCookiesToResponse = ({ res, user }) => {
+  const token = createJWT({ payload: user });
+
+  const thirtyDays = 30 * (1000 * 60 * 60 * 24);
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + thirtyDays),
+  });
+
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
 module.exports = {
   createJWT,
   isTokenValid,
+  attachCookiesToResponse,
 };
